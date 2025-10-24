30bdf9d52c989b61bec9d221ae148178e44610a8
Rahulguri13 <rahulverma950r@gmail.com>
2025-10-24 12:54:46 +0530

    button project

---
+++ java.js
@@ -0,0 +1,11 @@
+const modelBtn = document.querySelector(".model-btn");
+const model = document.querySelector(".model-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modelBtn.addEventListener("click", function(){
+model.classList.add("open-model");
+});
+
+closeBtn.addEventListener("click", function(){
+    model.classList.remove("open-model");
+})
